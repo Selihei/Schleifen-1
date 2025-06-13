15f1f3ff43c021fdc2c528475b3a0f0b5f95d014
Selihei <selina.heinrichlp@gmail.com>
2025-06-13 10:36:13 +0200

    added names for in loop

--- Aufgabe1.dart
+++ Aufgabe1.dart
@@ -2,6 +2,12 @@
 
 void main(){
 List<int> points = [4, 5, 4, 2, 6, 6, 3];
- List<String> names = ['Julietta', 'Benjamino', 'Hans-Günther',   
+List<String> names = ['Julietta', 'Benjamino', 'Hans-Günther',   
                          'Evalinea', 'Fiona', 'Gregory', 'Leonhart'];
+
+for(String name in names){
+    print('$name');
+}
+
+
 }
